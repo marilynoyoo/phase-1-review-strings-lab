c18de0f7d4e1feda66850d5f62c0e034389532f9
marilynakinyioyoo <marilyn.oyoo@student.moringaschool.com>
2024-06-27 00:30:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+const currentUser = 'Marilyn Oyoo';
+const welcomeMessage = `Welcome to Flatbook, ${'Marilyn Oyoo'}!`;
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, MARILYN OYOO!';
+const shortGreeting = `Welcome, ${'Marilyn Oyoo' .slice(0, 1)}!`;
+
+
+
+
+
+
+
+
